bb26cbaab9df162281ea57184f05daf5a4b403bb
David Benavidez <benavidez_david13@yahoo.com>
2020-08-31 09:44:55 -0700

    added production object to knex file

--- knexfile.js
+++ knexfile.js
@@ -13,38 +13,16 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  staging: {
-    client: 'pg',
-    connection: {
-      database: 'my_db',
-      user: process.env.USER,
-      password: process.env.PASSWORD,
-      DATABASE_URL: process.env.DATABASE_URL
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: "./database/migrations"
-    }
-  },
-
   production: {
     client: 'pg',
-    connection: {
-      user: process.env.USER,
-      password: process.env.PASSWORD,
-      DATABASE_URL: 'postgres://frneftcvosqcms:37ea7e005bc8b70bdde432d34dce240e2cc5a66324a8a8f3c1278109accca6cc@ec2-34-233-186-251.compute-1.amazonaws.com:5432/dcaqs1e7hlgmbe'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
       tableName: 'knex_migrations',
-      directory: __dirname + '/database/migrations',
+      directory: '/database/migrations',
     }
   }
 };
