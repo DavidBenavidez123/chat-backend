0647d42053e2356216b0760305eca3481d060aed
David Benavidez <benavidez_david13@yahoo.com>
2020-08-31 12:47:10 -0700

    removed production

--- knexfile.js
+++ knexfile.js
@@ -13,17 +13,17 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: './migrations',
-    }
-  }
+  // production: {
+  //   client: 'pg',
+  //   connection: process.env.DATABASE_URL,
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations',
+  //     directory: './migrations',
+  //   }
+  // }
 };
 
